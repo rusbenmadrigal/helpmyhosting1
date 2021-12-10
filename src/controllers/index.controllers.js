c113b912e7e9c8234c013bb2ef70da452a5d470c
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2021-12-09 19:10:10 -0600

    Update

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -8,5 +8,21 @@ indexCtrl.renderAbout = (req, res) => {
     res.render('about')
 };
 
+indexCtrl.renderBestHosting = (req, res) => {
+    res.render('best-hosting')
+};
+
+indexCtrl.renderRaiolaNetworks = (req, res) => {
+    res.render('raiola')
+};
+
+indexCtrl.renderBanaHosting = (req, res) => {
+    res.render('banahosting')
+};
+
+indexCtrl.renderHostiger = (req, res) => {
+    res.render('hostiger')
+};
+
 
 module.exports = indexCtrl;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,21 @@
-const { Router } = require('express')
+const { Router } = require("express");
 const router = Router();
 
-const { renderIndex, renderAbout } = require('../controllers/index.controllers')
+const {
+  renderIndex,
+  renderAbout,
+  renderBestHosting,
+  renderRaiolaNetworks,
+  renderBanaHosting,
+  renderHostiger,
+} = require("../controllers/index.controllers");
 
-router.get('/', renderIndex);
+router.get("/", renderIndex);
 
-router.get('/about', renderAbout);
-
+router.get("/about", renderAbout);
+router.get("/mejor-hosting", renderBestHosting);
+router.get("/mejor-hosting/raiola-networks", renderRaiolaNetworks);
+router.get("/mejor-hosting/banahosting", renderBanaHosting);
+router.get("/mejor-hosting/hostiger", renderHostiger);
 
 module.exports = router;
