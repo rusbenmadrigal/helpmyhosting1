8ebbbe33fce42c71b2091436da1c124aab8f0560
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2021-12-10 08:25:46 -0600

    Update Folders

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -8,21 +8,42 @@ indexCtrl.renderAbout = (req, res) => {
     res.render('about')
 };
 
+// BEAFFILIATE HOSTING PAGES - FOLDER "AFF"
+
 indexCtrl.renderBestHosting = (req, res) => {
-    res.render('best-hosting')
+    res.render('../views/best-hosting-pages/best-hosting')
 };
 
 indexCtrl.renderRaiolaNetworks = (req, res) => {
-    res.render('raiola')
+    res.render('../views/aff/raiola')
 };
 
 indexCtrl.renderBanaHosting = (req, res) => {
-    res.render('banahosting')
+    res.render('../views/aff/banahosting.hbs')
 };
 
 indexCtrl.renderHostiger = (req, res) => {
-    res.render('hostiger')
+    res.render('../views/aff/hostiger')
 };
 
+// BEST HOSTING FOLER GENERAL - FOLDER "BEST-HOSTING-PAGES"
+indexCtrl.renderBHCheap = (req, res) => {
+    res.render('../views/best-hosting-pages/cheap')
+};
+
+indexCtrl.renderBHWordPress = (req, res) => {
+    res.render('../views/best-hosting-pages/wordpress')
+};
+
+// BEST HOSTING BY COUNTRY - FOLDER "BEST-HOSTING-COUNTRY"
+indexCtrl.renderBHChile = (req, res) => {
+    res.render('../views/best-hosting-country/chile.hbs')
+};
+indexCtrl.renderBHEspana = (req, res) => {
+    res.render('../views/best-hosting-country/espana')
+};
+indexCtrl.renderBHMexico = (req, res) => {
+    res.render('../views/best-hosting-country/mexico')
+};
 
 module.exports = indexCtrl;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -8,14 +8,32 @@ const {
   renderRaiolaNetworks,
   renderBanaHosting,
   renderHostiger,
+  renderBHWordPress,
+  renderBHCheap,
+  renderBHChile,
+  renderBHEspana,
+  renderBHMexico,
+
 } = require("../controllers/index.controllers");
 
-router.get("/", renderIndex);
 
-router.get("/about", renderAbout);
-router.get("/mejor-hosting", renderBestHosting);
-router.get("/mejor-hosting/raiola-networks", renderRaiolaNetworks);
-router.get("/mejor-hosting/banahosting", renderBanaHosting);
-router.get("/mejor-hosting/hostiger", renderHostiger);
+// GENERAL PAGES FOLDER - /VIEWS/
+router.get("/", renderIndex);
+router.get("/about/", renderAbout);
+
+// BEST HOSTING URLS BY AFFILIATE FOLDER - /AFF/
+router.get("/mejor-hosting/", renderBestHosting);
+router.get("/mejor-hosting/raiola-networks/", renderRaiolaNetworks);
+router.get("/mejor-hosting/banahosting/", renderBanaHosting);
+router.get("/mejor-hosting/hostiger/", renderHostiger);
+
+// BEST HOSTING URLS GENERAL FOLDER - /BEST-HOSTING-PAGES/
+router.get("/mejor-hosting/wordpress/", renderBHWordPress);
+router.get("/mejor-hosting/barato/", renderBHCheap);
+
+// BEST HOSTING BY COUNTY FOLDER - /BEST-HOSTING-COUNTRY/
+router.get("/mejor-hosting/chile/", renderBHChile);
+router.get("/mejor-hosting/espana/", renderBHEspana);
+router.get("/mejor-hosting/mexico/", renderBHMexico);
 
 module.exports = router;
